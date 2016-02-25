8009b4a944b11d54d1a44b188ed762949fdc0a05
Guillaume Bersac <bersac_1@hotmail.fr>
2016-02-25 15:33:18 +0100

    remove test compute_weight_no_time

--- src/solver/test_solver.rs
+++ src/solver/test_solver.rs
@@ -5,32 +5,6 @@ use solver::resource_list::ResourceList;
 use solver::end_process_stack::EndProcessStack;
 use std;
 
-fn test_one_compute_weight(file: &str, resources: Vec<usize>, expected: i32) {
-    let project = Rc::new(Project::from_file(file, 1));
-    println!("project {:?}", project);
-    let resource_list = ResourceList::new(resources);
-    let (weight, _) = Node::new(project.clone(),
-                                2,
-                                resource_list,
-                                EndProcessStack::new(project.clone()));
-    println!("expected {} found {}", expected, weight);
-    assert!(weight == expected);
-}
-
-#[test]
-fn test_compute_weight_no_time() {
-    test_one_compute_weight("inputs/pomme",
-                            vec![100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-                                 100, 100, 100],
-                            100);
-    test_one_compute_weight("inputs/pomme",
-                            vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            0);
-    test_one_compute_weight("inputs/pomme",
-                            vec![0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            100);
-}
-
 /// Launching `nb_process` process of index `i_process` must consume all the
 /// resources of  `resources` and consume `time` turn
 fn test_one_resource_list(file: &str,
